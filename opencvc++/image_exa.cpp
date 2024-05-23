9e3638a209c722268b479ba15166db9dc75d43ce
markram1729 <rishikramjallarapu@gmail.com>
2024-05-23 14:21:29 +0530

    opencv examples for two images checks

--- opencvc++/image_exa.cpp
+++ opencvc++/image_exa.cpp
@@ -7,9 +7,9 @@
 using namespace cv;
 int main()
 {
-	std::string image_path=samples::findFile("/home/regaltos/Pictures/photo_2019-09-18_18-21-23.jpg");
+	//std::string image_path=samples::findFile("/home/regaltos/Pictures/photo_2019-09-18_18-21-23.jpg");
 	Mat img = imread("/home/regaltos/Pictures/photo_2019-09-18_18-21-23.jpg",IMREAD_COLOR);
-
+	std::cout <<"Dimension1 "<<img.dims<<std::endl;
 	if(img.empty())
 	{
 		std::cout<<"Coiuld not find image "<<std::endl;
@@ -23,5 +23,8 @@ int main()
 	{
 		imwrite("photo_2019-09-18_18-21-23.jpg",img);
 	}
+	Mat im2 = imread("/home/regaltos/Pictures/RIshik_Ram.png",IMREAD_UNCHANGED);
+	std::cout <<"Dimension2 "<<im2.dims<<std::endl;
 	return 0;
+
 }
