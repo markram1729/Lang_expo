6037173c8502154f048239cebe2cc3fc88c6879d
markram1729 <rishikramjallarapu@gmail.com>
2024-06-18 20:13:23 +0530

    3 dimensional image

--- opencvc++/image_exa.cpp
+++ opencvc++/image_exa.cpp
@@ -9,7 +9,7 @@ int main()
 {
 	//std::string image_path=samples::findFile("/home/regaltos/Pictures/photo_2019-09-18_18-21-23.jpg");
 	Mat img = imread("/home/regaltos/Pictures/photo_2019-09-18_18-21-23.jpg",IMREAD_COLOR);
-	std::cout <<"Dimension1 "<<img.dims<<std::endl;
+	std::cout <<"Dimension1 "<<img.dims<<" channel "<<img.channels()<<std::endl;
 	if(img.empty())
 	{
 		std::cout<<"Coiuld not find image "<<std::endl;
@@ -23,8 +23,11 @@ int main()
 	{
 		imwrite("photo_2019-09-18_18-21-23.jpg",img);
 	}
-	Mat im2 = imread("/home/regaltos/Pictures/RIshik_Ram.png",IMREAD_UNCHANGED);
-	std::cout <<"Dimension2 "<<im2.dims<<std::endl;
+	Mat im2 = imread("/home/regaltos/Pictures/RIshik_Ram.png",IMREAD_COLOR);
+	std::cout <<"Dimension2 "<<im2.dims<<" Img dd "<<im2.channels()<<std::endl;
+
+	Mat im3gray = imread("/home/regaltos/dev/llvm_dev/buddy_exa/Img_dir/YuTu.jpg",IMREAD_GRAYSCALE);
+	std::cout<<"JEN " <<im3gray.channels()<<"\n"<<im3gray<<std::endl;
 	return 0;
 
 }
